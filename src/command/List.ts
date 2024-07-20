288f3e59990b3c5d4a03e8b1b4ea7ad99e0ddbb4
gimsangjun <5321913@naver.com>
2024-07-21 02:59:28 +0900

    feat: 명령어 List 보기

--- src/command/List.ts
+++ src/command/List.ts
@@ -1,10 +1,19 @@
 import { Command } from ".";
-// TODO: 구현해야함.
+
+// 현재 명령어 목록 가져오기
 export class ListCommand implements Command {
   name = "list";
-  description = "Get the list of an commands";
+  description = "Get the list of commands";
+  private commands: Command[];
 
-  constructor() {}
+  constructor(commands: Command[]) {
+    this.commands = commands;
+  }
 
-  execute = async (args: string[]): Promise<void> => {};
+  execute = async (args: string[]): Promise<void> => {
+    console.log("Available commands:");
+    this.commands.forEach((command) => {
+      console.log(`- ${command.name}: ${command.description}`);
+    });
+  };
 }
--- src/command/index.ts
+++ src/command/index.ts
@@ -3,3 +3,7 @@ export interface Command {
   description: string;
   execute: (args: string[]) => Promise<void>;
 }
+
+export { PriceCommand } from "./Price";
+export { PriceAlertCommand } from "./PriceAlert";
+export { ListCommand } from "./List";
